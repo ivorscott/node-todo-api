dddb88a002327b4f9fc5fd9f9a184b17ced1ca65
Ivor Scott <ivor@devpie.co>
2017-02-09 23:18:05 -0500

    Added DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const {ObjectID} = require('mongodb'),
 let = app = express()
 app.use(bodyParser.json())
 
-app.post('/todos', (req, res) => {
+app.post('/todos', (req,res) => {
   let text = req.body.text,
       todo = new Todo({text})
 
@@ -19,19 +19,29 @@ app.post('/todos', (req, res) => {
   },(e) => res.status(400).send(e))
 })
 
-app.get('/todos', (req, res) => {
+app.get('/todos', (req,res) => {
   Todo.find().then((todos) => {
     res.send({todos})
   },(e) => res.status(400).send(e))
 })
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', (req,res) => {
   let id = req.params.id
   if(!ObjectID.isValid(id)) return res.status(404).send()
 
   Todo.findById(id).then((todo) => {
     if(!todo) return res.status(404).send()
-    res.send({todo});
+    res.send({todo})
+  }).catch((e) => res.status(400).send())
+})
+
+app.delete('/todos/:id', (req,res) => {
+  let id = req.params.id
+  if(!ObjectID.isValid(id)) return res.status(404).send()
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) return res.status(404).send()
+    res.send({todo})
   }).catch((e) => res.status(400).send())
 })
 
